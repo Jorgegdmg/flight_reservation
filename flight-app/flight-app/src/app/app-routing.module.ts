ba55bf9af6734f029daa9a3d3cc49eb7aaf3282b
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2025-08-02 17:24:33 +0200

    Apartados del Formularios terminados: HTML y CSS. Falta el componente TS

--- flight-app/flight-app/src/app/app-routing.module.ts
+++ flight-app/flight-app/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightListComponent } from './components/flight-list/flight-list.component';
+import { FlightFormComponent } from './components/flight-form/flight-form.component';
 
 const routes: Routes = [
-  { path: '', component: FlightListComponent }
+  { path: 'listado', component: FlightListComponent },
+  { path: 'form', component: FlightFormComponent}
 ];
 
 @NgModule({
--- flight-app/flight-app/src/app/app.module.ts
+++ flight-app/flight-app/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { FlightListComponent } from './components/flight-list/flight-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlightFormComponent } from './components/flight-form/flight-form.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent,         // tu componente raíz clásico
-    FlightListComponent   // tu componente de lista
+    AppComponent,
+    FlightListComponent,
+    FlightFormComponent
+     
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
