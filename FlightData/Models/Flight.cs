adae891d50db3d2eb7dc3e1896b927ad8836f98e
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2025-08-09 18:00:08 +0200

    añadidos nuevos parametros al modelo de Flight

--- FlightData/Models/Flight.cs
+++ FlightData/Models/Flight.cs
@@ -9,6 +9,13 @@ namespace FlightData.Models
         public string Origin { get; set; } = default!;
         public string Destination { get; set; } = default!;
         public DateTime DepartureTime { get; set; }
+        public int Capacity { get; set; } = default!;
+        public bool IsDirect { get; set; } = default!;
+        public string CabinClass { get; set; } = default!;
+        public int Pasengers { get; set; } = default!;
+        public string TripType { get; set; } = default!;
+
+
 
         public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 
